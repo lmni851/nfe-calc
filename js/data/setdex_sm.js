8c9736363a14f802ffd29c7e07c394f7a8717f24
lmni851 <40423374+lmni851@users.noreply.github.com>
2018-06-20 16:38:58 +0800

    Create setdex_sm.js

---
+++ js/data/setdex_sm.js
@@ -0,0 +1,20 @@
+var SETDEX_SM = {
+	"Clefairy": {
+		"NFE Calm Mind": {"level": 100, "evs": {"hp": 252, "df": 252, "sd": 4}, "nature": "Bold", "ability": "Magic Guard", "item": "Eviolite", "moves": ["Calm Mind", "Moonblast", "Fire Blast", "Soft-Boiled"]},
+		"NFE Support": {"level": 100, "evs": {"hp": 252, "df": 252, "sd": 4}, "nature": "Bold", "ability": "Magic Guard", "item": "Eviolite", "moves": ["Stealth Rock", "Moonblast", "Heal Bell", "Soft-Boiled"]},
+	"Combusken": {
+		"NFE Special Sweeper": {"level": 100, "evs": {"sa": 252, "sp": 252, "sd": 4}, "nature": "Modest", "ability": "Speed Boost", "item": "Fightinium Z", "moves": ["Focus Blast", "Protect", "Fire Blast", "Defog"]},
+		"NFE Physical Sweeper": {"level": 100, "evs": {"at": 252, "sp": 252, "sd": 4}, "nature": "Adamant", "ability": "Speed Boost", "item": "Firium Z", "moves": ["Swords Dance", "Protect", "Flare Blitz", "Sky Uppercut"]},
+	"Eevee": {
+		"NFE Special Sweeper": {"level": 100, "evs": {"sa": 252, "sp": 252, "sd": 4}, "nature": "Modest", "ability": "Speed Boost", "item": "Eevium Z", "moves": ["Last Resort", "Stored Power", "Hyper Voice", "Substitute"]},
+	"Electabuzz": {
+		"NFE Pivot": {"level": 100, "evs": {"sa": 252, "sp": 252, "sd": 4}, "nature": "Timid", "ability": "Vital Spirit", "item": "Life Orb", "moves": ["Thunderbolt", "Volt Switch", "Focus Blast", "Psychic"]},
+	"Ferroseed": {
+		"NFE Wall": {"level": 100, "evs": {"hp": 252, "sd": 252, "df": 4}, "nature": "Sassy", "ability": "Iron Barbs", "item": "Eviolite", "moves": ["Leech Seed", "Knock Off", "Stealth Rock", "Thunder Wave"]},
+	"Frogadier": {
+		"NFE All Out Attacker": {"level": 100, "evs": {"sa": 252, "sp": 252, "sd": 4}, "nature": "Timid", "ability": "Protean", "item": "Life Orb", "moves": ["Hydro Pump", "Dark Pulse", "Ice Beam", "U-Turn"]},
+		"NFE Choice Scarf": {"level": 100, "evs": {"sa": 252, "sp": 252, "sd": 4}, "nature": "Timid", "ability": "Protean", "item": "Choice Scarf", "moves": ["Hydro Pump", "Dark Pulse", "Ice Beam", "U-Turn"]},
+		"NFE Lead": {"level": 100, "evs": {"sa": 252, "sp": 252, "at": 4}, "nature": "Timid", "ability": "Protean", "item": "Focus Sash", "moves": ["Toxic Spikes", "Taunt", "Hydro Pump", "U-Turn"]},
+	"Gloom": {
+		"NFE Physically,Defensive": {"level": 100, "evs": {"hp": 252, "df": 252, "sd": 4}, "nature": "Bold", "ability": "Stench", "item": "Eviolite", "moves": ["Strength Sap", "Sleep Powder", "Sludge Bomb", "Giga Drain"]},
+};
